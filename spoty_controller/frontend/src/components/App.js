6eb2c73a0d29a10adc904fcbfdbfcefa3b1d7a34
Aliaksei Slepau <aslepaugo@gmail.com>
2021-02-27 22:55:18 +0100

    added react router

--- spoty_controller/frontend/src/components/App.js
+++ spoty_controller/frontend/src/components/App.js
@@ -1,15 +1,22 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
+import HomePage from "./HomePage";
+
 
 export default class App extends Component{
     constructor(props){
-        super(props)
+        super(props);
+
     }
 
     render(){
-        return <h1>Test React Code</h1>;
+        return (
+            <div>
+                <HomePage />
+            </div>
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App/>, appDiv);
+render(<App />, appDiv);
